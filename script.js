6a77238540864c13e09b285af484a63647e9a486
Alex Crooks <alexcrooks2@gmail.com>
2021-09-07 17:00:28 -0700

    Working version with styling

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 class Player {
-  constructor(){
-    this.lives = 3
-    this.points = 0
+  constructor(highscore = 0){
+    this.lives = 3;
+    this.points = 0;
+    this.highscore = highscore;
   }
 
   loseLife() {
     this.lives--;
   }
 
-  winPoints() {
-    this.points++;
+  winPoints(points) {
+    this.points += points;
   }
 }
 
 class View {
  constructor(player, game, el) {
-   this.player = player;
-   this.game = game;
-   this.el = el;
+  this.player = player;
+  this.game = game;
+  this.el = el;
+  this.timeLeft = 7;
+  this.timeRemaining = true;  
   this.handleClick = this.handleClick.bind(this)
-  this.setupBoard()
+  this.createBoard()
   this.bindEvents()
-  
 }
 
-setupBoard() {
+createBoard() {
  // Display lives
+ document.getElementById("ui").value = null;
  const numLives = document.getElementById("lives")
- numLives.innerHTML = "";
+ numLives.innerHTML = "Lives: ";
  for(let i = 0; i < this.player.lives; i++) {
      numLives.innerHTML += '<img src = "https://restorationhemproject.org/wp-content/uploads/2018/07/heart-png-8.png">'
  }
@@ -35,7 +38,11 @@ setupBoard() {
  const numPoints = document.getElementById("points")
  numPoints.innerHTML = this.player.points;
  // Display word
+ let num= this.randomTimerForTimer(this.game.word.length + 2);
+ if(this.player.points === 0) num = 700;
  document.getElementById("wordToType").innerHTML = `${this.game.word}`
+ this.timer = new Countdown(num, this)
+ this.timer.start()
 }
 
 bindEvents() {
@@ -43,17 +50,40 @@ bindEvents() {
   this.el.addEventListener("keyup", this.handleClick)
 }
 
+randomTimerForTimer(max) {
+  return Math.floor(Math.random() * (max - 2) + 2) * 100
+}
+
+endOfGame() {
+  this.timer.stop()
+  if(this.timeRemaining && this.game.checkWord()) {
+    this.handleWinPoints();
+  }
+  else {
+    this.handleLostLife();
+    if(this.game.isOver(this.player)) {
+      this.initiateShutdown();
+      return
+    }
+  }
+  this.game = new Game();
+  this.createBoard();
+}
+
+initiateShutdown() {
+  document.getElementById("lives").innerHTML = "You have no lives left :("
+  document.getElementById("wordToType").innerHTML = "Game Over!"
+  document.getElementById("userInput").style.display = "none"
+  document.getElementById("play_again").style.display = "initial"
+}
+
 handleLostLife() {
   this.player.loseLife()
-  const numLives = document.getElementById("lives")
-  numLives.innerHTML = ""
-  for(let i = 0; i < this.player.lives; i++) {
-      numLives.innerHTML += '<img src = "https://restorationhemproject.org/wp-content/uploads/2018/07/heart-png-8.png">'
-  }
 }
 
 handleWinPoints() {
-  this.player.winPoints();
+  let pointsToAdd = (this.timeLeft * 100) - this.timer.usedTime;
+  this.player.winPoints(pointsToAdd);
   let numPoints = document.getElementById("points")
   numPoints.innerHTML = this.player.points;
 }
@@ -65,32 +95,59 @@ handleClick(e){
       document.getElementById("ui_enter").click();
       let wordToGuess = document.getElementById('ui');
       this.game.answer = wordToGuess.value
-      if(this.game.checkWord()) {
-        this.handleWinPoints();
-      }
-      else {
-        this.handleLostLife();
-      };
-      this.game = new Game();
-      this.resetPage()
-    }
+      this.endOfGame()}; 
   };
 }
-
-resetPage() {
-   // Display lives
-  const numLives = document.getElementById("lives")
-  numLives.innerHTML = "";
-  for(let i = 0; i < this.player.lives; i++) {
-    numLives.innerHTML += '<img src = "https://restorationhemproject.org/wp-content/uploads/2018/07/heart-png-8.png">'
-  }
- // Display points
-  const numPoints = document.getElementById("points")
-  numPoints.innerHTML = this.player.points;
- // Display word
-  document.getElementById("wordToType").innerHTML = `${this.game.word}`
-  document.getElementById("ui").value = null;
 }
+
+function Countdown(seconds, view) {
+  var that = {};
+
+  that.elem = document.getElementById("timerdiv");
+  that.seconds = seconds * 100;
+  that.totalTime = seconds;
+  that.usedTime = 0;
+  that.startTime = +new Date();
+  that.timer = null;
+
+  that.count = function() {
+    that.usedTime = Math.floor((+new Date() - that.startTime) / 10);
+
+    var tt = that.totalTime - that.usedTime;
+    if (tt <= 0) {
+      that.elem.innerHTML = '0';
+      view.timeRemaining = false;
+      view.endOfGame();
+      clearInterval(that.timer);
+    } else {
+      var ss = Math.floor(tt / 100) * 100;
+      var ms = tt - Math.floor(tt / 100) * 100;
+
+      that.elem.innerHTML = `${ss + ms}`;
+    }
+  };
+  
+  that.init = function() {
+    if(that.timer){
+      clearInterval(that.timer);
+      that.totalTime = seconds * 100;
+      that.usedTime = 0;
+      that.startTime = +new Date();
+      that.timer = null;
+    }
+  };
+
+  that.start = function() {
+    if(!that.timer){
+       that.timer = setInterval(that.count, 10);
+    }
+  };
+
+  that.stop = function() {
+    if (that.timer) clearInterval(that.timer);
+  };
+
+  return that;
 }
 
 function Game() {
@@ -104,10 +161,6 @@ Game.STARTERWORDS = ["dog", "cat", "frog", "bongo"]
 
 
 // Get 100 random words from API
-Game.prototype.constructMiniGame = function() {
-
-}
-
 Game.prototype.checkWord = function(){
   return this.word === this.answer
 }
@@ -119,11 +172,12 @@ Game.prototype.getRandomWord = function() {
   return Game.STARTERWORDS[ranNum]
 }
 
-Game.prototype.isOver = function() {
-  if(this.lives === 0) {
-      const playerSpace = document.getElementById("userInput") 
-      playerSpace.innerHTML = "You have zero lives :( <br> Play again?"
+Game.prototype.isOver = function(player) {
+  if(player.lives === 0) {
+    return true
   }
+  return false;
+ 
 }
 
 document.addEventListener("DOMContentLoaded", () => {
